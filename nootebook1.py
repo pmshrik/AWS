852a0486e6d5e86d5ec6be81d8480aedf19bec1b
pmshrik <142974574+pmshrik@users.noreply.github.com>
2023-08-23 18:06:28 +0530

    Add files via upload

---
+++ nootebook1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+import numpy as np
+arr2 = np.array([[1,2,3],[4,5,6]])
+arr2.ndim
+
+
+# In[15]:
+
+
+import numpy as np
+arr1 = np.array([1,2,3])
+arr1.ndim
+arr2 = np.array([[1,2,3],[4,5,6]])
+arr2.ndim
+
+
+# In[16]:
+
+
+import numpy as np
+arr1 = np.array([1,2,3])
+arr1.ndim
+arr2 = np.array([[1,2,3],[4,5,6]])
+arr2.ndim
+arr3 = np.array([[[1,2,3],[4,5,6],[7,8,9]]])
+arr3.ndim
+
+
+# In[ ]:
+
+
+
+
